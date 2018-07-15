85866baad16a898506ee83471f8fde6fe5da75ed
Niklas von Maltzahn <niklasvm@gmail.com>
2018-07-15 15:25:54 +0200

    expose port 38383

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 
 sudo docker stop rpi;
 sudo docker rm rpi;
-sudo docker run -ti -d --name rpi rpi_docker;
+sudo docker run -ti -d --name rpi -p 3838:3838 rpi_docker;
 sudo docker exec -ti rpi bash;
